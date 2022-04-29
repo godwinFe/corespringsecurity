d02dcb42ae845daf365fad33b7f746f734d4a79f
godwinFe <sinseungja@naver.com>
2022-04-29 15:03:35 +0900

    Initial commit

---
+++ src/main/java/io/security/corespringsecurity/controller/user/UserController.java
@@ -0,0 +1,20 @@
+package io.security.corespringsecurity.controller.user;
+
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/mypage")
+    public String myPage(){
+        return "user/mypage";
+    }
+
+
+}
